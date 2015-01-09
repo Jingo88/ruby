ccdecbf5d5755a00b428b1179d8171bf263f87a8
Jason <Jason@Jasons-MacBook-Air.local>
2015-01-09 15:42:10 -0500

    initial stage

--- game.rb
+++ game.rb
@@ -1,4 +1,70 @@
-hp = 10
+# array of monsters
+# array of weapons
+# array of attacks
+# random weapon attack damage
+# run or fight
+# experience points and levels
+# hp points and levels
 
-puts "pick a name for yo hero!"
-hero = gets.chomp
+monsters = ['Mutated Octopus', 'Sabertooth Goldfish', 'Lady Gaga', 'Hannah Montana']
+monsterAtk = ['Slap', 'Bite', 'Eyepoke']
+
+hero = {
+	'health' => 10
+}
+
+puts "Welcome Adventurer, state your name if intend to join this quest"
+
+hero["name"] = gets.chomp.capitalize
+
+puts "Hello #{hero["name"]} let us slay! Choose your weapon, I have a sword, or spear, or axe"
+
+hero["weapon"] = gets.chomp.capitalize
+
+if hero['weapon'] == 'Sword'
+	hero['atkrange'] = '2 to 5'
+
+elsif hero['weapon'] == 'Spear'
+	hero['atkrange'] = '1 to 6'
+
+elsif hero['weapon'] == 'Axe'
+	hero['atkrange'] = '3 to 4'
+
+else
+	puts "I do not have that in my inventory, please choose a sword, or spear, or axe"
+
+end
+
+puts hero
+
+puts "Your weapon of choice is the #{hero['weapon']} your attack damage range is #{hero['atkrange']}"
+puts "Your current health is #{hero['health']}"
+
+
+
+
+
+
+
+
+
+
+
+
+
+puts 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
